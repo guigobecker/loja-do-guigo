9eb3cdb86a9b9b52f1cfed82284ca1c376a0f0f5
Guigo <rsbecker@inf.ufsm.br>
2023-12-01 13:54:38 -0300

    Corrigir requisição

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,15 +1,19 @@
 <script setup>
-    import { computed, onMounted } from 'vue';
+    import { computed, onMounted, ref } from 'vue';
     import store from '../store';
+
+    const produtos = ref([]);
 
     onMounted(() => {
         fetch("https://fakestoreapi.com/products")
           .then((res) => res.json())
+          .then((json) => (produtos.value = json))
           .then((json) => console.log(json));
     });
 </script>
 
 <template>
     <div>
+        <pre>{{ produtos }}</pre>
     </div>
 </template>
